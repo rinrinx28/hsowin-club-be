8158923cf0de51d8e279910308eb08e8ff69ccd9
Rinx28 <minhanhpreeip@gmail.com>
2024-07-21 20:39:02 +0700

    Update Cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Define an array of allowed origins
+  const allowedOrigins = ['http://localhost:3000'];
+
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  });
   await app.listen(3031);
 }
 bootstrap();
