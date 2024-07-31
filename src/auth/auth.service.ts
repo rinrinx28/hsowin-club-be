2df2e006049c13e827bc167528624af70b1825e3
Rinx28 <minhanhpreeip@gmail.com>
2024-08-01 03:27:01 +0700

    Debugs Limited IP 4

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -20,11 +20,6 @@ export class AuthService {
 
   async signIn(username: string, pass: string, req: any) {
     const ip_address = req.headers['x-real-ip'];
-    const targetIp = await this.userService.handleUserWithIp(ip_address);
-    if (targetIp.countAccount.length > 1)
-      throw new UnauthorizedException(
-        'Nghi vấn spam, bạn không thể đăng nhập thêm tài khoản ở trên thiết bị này',
-      );
     const user = await this.userService.findOne(username);
     if (!user || user.pwd_h !== pass) {
       throw new UnauthorizedException('Username hoặc password không đúng');
@@ -34,6 +29,16 @@ export class AuthService {
     const userObj = user.toObject(); // Convert Mongoose document to plain object
     const { pwd_h, ...res } = userObj;
     if (userObj.isBan) throw Error('Tài khoản đã bị banned');
+
+    const targetIp = await this.userService.handleUserWithIp(ip_address);
+    if (
+      targetIp &&
+      !targetIp.countAccount.includes(user.id) &&
+      targetIp.countAccount.length > 1
+    )
+      throw new UnauthorizedException(
+        'Nghi vấn spam, bạn không thể đăng nhập thêm tài khoản ở trên thiết bị này',
+      );
     const access_token = await this.jwtService.signAsync(payload);
     await this.autTokenhModel.create({ token: access_token, isEnd: false });
     await this.userService.handleUserUpdateIp(user.id, ip_address);
@@ -48,7 +53,7 @@ export class AuthService {
   async signUp(body: CreateAuthDto, req: any) {
     const ip_address = req.headers['x-real-ip'];
     const targetIp = await this.userService.handleUserWithIp(ip_address);
-    if (targetIp.countAccount.length > 1)
+    if (targetIp && targetIp.countAccount.length > 1)
       throw new UnauthorizedException(
         'Nghi vấn spam, bạn không thể tạo thêm tài khoản ở trên thiết bị này',
       );
@@ -64,16 +69,20 @@ export class AuthService {
     const ip_address = req.headers['x-real-ip'];
     this.logger.log(`[Relogin] UID:${token?.sub} - IP:${ip_address}`);
     try {
+      const user = await this.userService.findById(token?.sub);
+      const new_date = user.toObject();
+      if (new_date.isBan) throw Error('Tài khoản đã bị banned');
       const targetIp = await this.userService.handleUserWithIp(ip_address);
-      if (targetIp.countAccount.length > 1)
+      if (
+        targetIp &&
+        !targetIp.countAccount.includes(user.id) &&
+        targetIp.countAccount.length > 1
+      )
         throw new UnauthorizedException(
           'Nghi vấn spam, bạn không thể đăng nhập thêm tài khoản ở trên thiết bị này',
         );
-      const user = await this.userService.findById(token?.sub);
-      const new_date = user.toObject();
       await this.userService.handleUserUpdateIp(token?.sub, ip_address);
       await this.userService.handleAddIp(token?.sub, ip_address);
-      if (new_date.isBan) throw Error('Tài khoản đã bị banned');
       delete new_date.pwd_h;
       return new_date;
     } catch (err) {
