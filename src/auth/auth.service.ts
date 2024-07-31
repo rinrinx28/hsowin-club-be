344c917d9630fe9e56e4e9373d20082b0108b111
Rinx28 <minhanhpreeip@gmail.com>
2024-08-01 02:44:21 +0700

    Update IP table

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -17,7 +17,7 @@ export class AuthService {
   ) {}
 
   async signIn(username: string, pass: string, req: any) {
-    console.log(req);
+    const ip = req.headers['x-real-ip'];
     const user = await this.userService.findOne(username);
     if (!user || user.pwd_h !== pass) {
       throw new UnauthorizedException('Username hoặc password không đúng');
@@ -29,6 +29,8 @@ export class AuthService {
     if (userObj.isBan) throw Error('Tài khoản đã bị banned');
     const access_token = await this.jwtService.signAsync(payload);
     await this.autTokenhModel.create({ token: access_token, isEnd: false });
+    await this.userService.handleUserUpdateIp(ip, user.id);
+    await this.userService.handleAddIp(user.id, ip);
     return {
       access_token: access_token,
       user: res,
@@ -36,17 +38,21 @@ export class AuthService {
   }
 
   async signUp(body: CreateAuthDto, req: any) {
-    console.log(req);
+    const ip = req.headers['x-real-ip'];
     const result = await this.userService.create(body);
+    await this.userService.handleUserUpdateIp(ip, result.id);
+    await this.userService.handleAddIp(result.id, ip);
     delete result.pwd_h;
     return result;
   }
 
   async relogin(token: string, req: any) {
-    console.log(req);
+    const ip = req.headers['x-real-ip'];
     try {
       const user = await this.userService.findById(token?.sub);
       const new_date = user.toObject();
+      await this.userService.handleUserUpdateIp(ip, token?.sub);
+      await this.userService.handleAddIp(token?.sub, ip);
       if (new_date.isBan) throw Error('Tài khoản đã bị banned');
       delete new_date.pwd_h;
       return new_date;
